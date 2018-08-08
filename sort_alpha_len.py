5b6684ed88a9892e7c9d4d1031833df1d7e52444
SteephanSteve <40833438+SteephanSteve@users.noreply.github.com>
2018-08-08 18:56:46 +0530

    Update sort_alpha_len.py

--- sort_alpha_len.py
+++ sort_alpha_len.py
@@ -4,10 +4,14 @@ n=int(input())
 a=raw_input().split(" ")
 a.sort()
 l=[]
+s=[]
 for i in a:
     l.append(len(i))
 l.sort()
 for i in l:
     for j in a:
         if len(j)==i:
-            print j,
+            if j not in s:
+                s.append(j)
+for i in s:
+    print i,
